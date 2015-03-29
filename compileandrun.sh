01bf67795a629d6348cc12b4cf47323f1696a94b
Jake Martin <jmart.alb@gmail.com>
2015-03-29 16:33:25 -0400

    initial commit

---
+++ compileandrun.sh
@@ -0,0 +1,2 @@
+gcc -Wall mybackup.c
+./a.out
---
+++ mybackup.c
@@ -0,0 +1,17 @@
+//Jacob Martin
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+int main() {
+  mkdir("test",1777);
+  return 1;
+}
